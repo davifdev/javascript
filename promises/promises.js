4d002d81692e3c37f40a74d1c593660809f748fc
Davi Fernandes <davi.fer159@gmail.com>
2025-04-07 12:34:30 -0300

    review assync

--- promises/promises.js
+++ promises/promises.js
@@ -1,29 +1,35 @@
 function rand(min, max) {
-  max *= 1000;
   min *= 1000;
+  max *= 1000;
+
   return Math.floor(Math.random() * (max - min) + min);
 }
-
-function esperaAi(msg, time) {
+function esperaAi(frase, tempo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof msg !== "string") reject(new TypeError("Tipo inválido!"));
-      resolve(msg);
-    }, time);
+      if (typeof frase !== "string")
+        return reject(new TypeError("Tipo Inválido"));
+      resolve(frase);
+    }, tempo);
   });
 }
 
-esperaAi("Frase 1", rand(1, 3))
-  .then((response) => {
-    console.log(response);
-    return esperaAi("Frase 2", rand(1, 3));
-  })
-  .then((response) => {
-    console.log(response);
-    return esperaAi("Frase 3", rand(1, 3));
-  })
-  .then((response) => {
-    console.log(response);
-  })
-  .finally(() => console.log("Terminou!"))
-  .catch((err) => console.log(err));
+async function executaFrase() {
+  try {
+    const frase1 = await esperaAi("Frase 1", rand(1, 3));
+    console.log(frase1);
+    const frase2 = await esperaAi("Frase 2", rand(1, 3));
+    console.log(frase2);
+    const frase3 = await esperaAi(3, rand(1, 3));
+    console.log(frase3);
+    const frase4 = await esperaAi("Frase 4", rand(1, 3));
+    console.log(frase4);
+    const frase5 = await esperaAi("Frase 5", rand(1, 3));
+    console.log(frase5);
+    console.log("Terminou");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+executaFrase();
