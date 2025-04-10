b7b38086b8b2ea2fa3e6b1a0cec6718d043a262c
Davi Fernandes <davi.fer159@gmail.com>
2025-04-10 10:11:54 -0300

    typing

---
+++ basicsJavascript/strongTyping.ts
@@ -0,0 +1,12 @@
+// Em linguagens fortemente tipadas, devemos definir o tipo da variável em sua declaração
+
+const name: string = "Davi Fernandes";
+const number: number = 10;
+const arr: Array<string | number> = ["Davi", "Fernandes", 24];
+const obj: Object = {
+  name: "Davi",
+  age: 24,
+  role: "Fullstack Developer",
+};
+
+export default name;
---
+++ basicsJavascript/weakTyping.js
@@ -0,0 +1,10 @@
+// Em linguagens de tipagem fraca não é necessário declarar o tipo da variável ela faz isso automaticamente
+
+const name = "Davi Fernandes";
+const age = 24;
+const arr = ["Davi", "Fernandes", 24];
+const obj = {
+  name: "Davi",
+  lastName: "Fernandes",
+  age: 24,
+};
