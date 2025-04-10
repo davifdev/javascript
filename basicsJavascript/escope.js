c29998fb2871b6965e972470ac7213d11d8d7c11
Davi Fernandes <davi.fer159@gmail.com>
2025-04-10 09:59:35 -0300

    escope

---
+++ basicsJavascript/escope.js
@@ -0,0 +1,11 @@
+// Escopo das variáveis, se refere ao local em que podemos acessar a variável no algoritimo
+
+// Escopo global
+var name = "Davi";
+console.log(name);
+
+{
+  // Escopo de bloco
+  var name = "Luiz";
+  console.log(name);
+}
