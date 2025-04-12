ca81aef0ea56dd4620a8f08bb111bac7ada7ebd6
Davi Fernandes <davi.fer159@gmail.com>
2025-04-12 11:03:32 -0300

    const mutable

---
+++ basicsJavascript/constMutable.js
@@ -0,0 +1,12 @@
+// Variáveis são uma espécie de alias para mencionar algum valor que está salvo memória
+// Os valores são dados que realmente ficam salvos namemória e sustentam um determinado tipo
+// Tipos primitivos são imutáveis
+// Tipos derivados são mutáveis
+// Valores mutáveis geralmente são estruturas de dados mais complexas que sustentam valores ou comportamentos internamente
+
+const num = 10;
+
+const arr = ["a", "b", "c"];
+arr[1] = "c";
+
+console.log(arr);
