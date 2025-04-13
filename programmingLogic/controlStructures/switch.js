3c1c8cfc5b75f21e69df62706f865bf5dd8428ac
Davi Fernandes <davi.fer159@gmail.com>
2025-04-13 12:18:48 -0300

    switch

---
+++ programmingLogic/controlStructures/switch.js
@@ -0,0 +1,27 @@
+const date = new Date();
+const dayOfWeek = date.getDay();
+
+switch (dayOfWeek) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Segunda-feira");
+    break;
+  case 2:
+    console.log("Terça-feira");
+    break;
+  case 3:
+    console.log("Quarta-feria");
+    break;
+  case 4:
+    console.log("Quinta-feira");
+    break;
+  case 5:
+    console.log("Sexta-feira");
+    break;
+  case 6:
+    console.log("Sábado");
+  default:
+    console.log("Dia não encontrado!");
+}
